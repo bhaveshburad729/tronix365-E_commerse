4c51a1b630058eba2b5e1176d61e565497da246b
bhaveshburad729 <bhaveshburad729@gmail.com>
2026-01-28 10:39:30 +0530

    added all routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import AdminDashboard from './pages/AdminDashboard'; // We will create this
+import Signup from './pages/Signup';
+import AdminDashboard from './pages/AdminDashboard';
+import Shop from './pages/Shop';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+import Wishlist from './pages/Wishlist';
+import { CartProvider } from './context/CartContext';
+import { WishlistProvider } from './context/WishlistContext';
 import { useEffect } from 'react';
 
 // Wrapper to scroll to top on route change
@@ -26,22 +33,28 @@ const Placeholder = ({ title }) => (
 function App() {
   return (
     <Router>
-      <ScrollToTop />
-      <div className="min-h-screen bg-tronix-bg text-tronix-text font-sans selection:bg-tronix-primary selection:text-white flex flex-col">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Placeholder title="Shop" />} />
-            <Route path="/categories" element={<Placeholder title="Categories" />} />
-            <Route path="/cart" element={<Placeholder title="Cart" />} />
-            <Route path="/wishlist" element={<Placeholder title="Wishlist" />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <CartProvider>
+        <WishlistProvider>
+          <ScrollToTop />
+          <div className="min-h-screen bg-tronix-bg text-tronix-text font-sans selection:bg-tronix-primary selection:text-white flex flex-col">
+            <Navbar />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
+                <Route path="/product/:id" element={<ProductDetails />} />
+                <Route path="/categories" element={<Shop />} /> {/* Re-using Shop for now, or could pass category prop */}
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/wishlist" element={<Wishlist />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/admin" element={<AdminDashboard />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </WishlistProvider>
+      </CartProvider>
     </Router>
   );
 }
